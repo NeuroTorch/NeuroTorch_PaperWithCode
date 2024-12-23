befe0f69d0cd49822c9b34d1752d241ce8908995
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-12-22 22:14:02 -0500

    Create gen_mnist_results.py

---
+++ images_classification/gen_mnist_results.py
@@ -0,0 +1,32 @@
+import torch
+from pythonbasictools.device import log_device_setup, DeepLib
+from pythonbasictools.logging_tools import logs_file_setup
+
+from dataset import DatasetId
+from images_classification.train_and_test_script import run
+
+if __name__ == '__main__':
+	logs_file_setup("mnist", add_stdout=False)
+	log_device_setup(deepLib=DeepLib.Pytorch)
+	if torch.cuda.is_available():
+		torch.cuda.set_per_process_memory_fraction(0.9)
+	
+	dataset_id = DatasetId.MNIST
+	
+	run(
+		dataset_id=dataset_id,
+		
+		# Training parameters
+		n_iterations=30,
+		batch_size=256,
+		learning_rate=2e-4,
+		
+		# Network parameters
+		n_steps=100,
+		n_hidden_neurons=100,
+		dt=1e-3,
+		
+		# Other parameters
+		seed=0,
+		force_overwrite=False,
+	)
